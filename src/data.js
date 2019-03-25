9fd2afd288cb10c3df2fefe264a5f890eb2236f2
Jan Borchers <humancatfood@users.noreply.github.com>
2019-03-25 10:47:09 +0000

    simplify example rendering

--- src/data.js
+++
@@ -1,7 +0,0 @@
-
-const DATA_URL = 'https://s3.amazonaws.com/cdn.clearscore.com/native/interview_test/creditReportInfo.json'
-
-
-export const fetchReportInfo = () => fetch(DATA_URL)
-  .then(response => response.json())
-  .catch(console.error.bind(null, 'error fetching data:'));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import ExampleScoreIndicator from './ScoreIndicator/example';
+
+
+import ScoreIndicator from './ScoreIndicator';
+
+import creditReportInfo from './creditReportInfo.json';
 
 
 
-ReactDOM.render(<ExampleScoreIndicator />, document.getElementById('root'));
+const slides = [
+  {
+    render: () => (
+      <div>
+        <h1>Score Indicator</h1>
+        <div>{creditReportInfo.creditReportInfo.score}</div>
+      </div>
+    )
+  },
+  {
+    render: () => (
+      <div>
+        <h1>Long Term Dept</h1>
+        <div>{creditReportInfo.creditReportInfo.currentLongTermDebt}</div>
+      </div>
+    )
+  }
+];
+
+
+ReactDOM.render(<ScoreIndicator slides={slides} />, document.getElementById('root'));
