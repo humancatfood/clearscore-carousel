6da901aacca623dfd2ad0844c0548882d4e85c84
Jan Borchers <humancatfood@users.noreply.github.com>
2019-03-26 21:32:55 +0000

    Updates

---
+++ precache-manifest.32afae104929e2ccfae5fba8b478cbe2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e064f15ca05dfb23b49e52dffeb75c1",
+    "url": "/clearscore-carousel/static/media/app-background.9e064f15.png"
+  },
+  {
+    "revision": "380f56dbc83f11fea0aa",
+    "url": "/clearscore-carousel/static/js/runtime~main.efbe80ca.js"
+  },
+  {
+    "revision": "b85d5851e6253c3bad7b",
+    "url": "/clearscore-carousel/static/js/main.2f000aad.chunk.js"
+  },
+  {
+    "revision": "657c38777fb417751f03",
+    "url": "/clearscore-carousel/static/js/2.08853022.chunk.js"
+  },
+  {
+    "revision": "b85d5851e6253c3bad7b",
+    "url": "/clearscore-carousel/static/css/main.ead194a9.chunk.css"
+  },
+  {
+    "revision": "6214e97de038ff35a4994404c9e6f3f0",
+    "url": "/clearscore-carousel/index.html"
+  }
+];
